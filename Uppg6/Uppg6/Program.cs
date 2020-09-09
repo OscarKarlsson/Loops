0fdc5c4b4c527803e624eeda7e14aecc95f5a96b
OscarKarlsson <70796269+OscarKarlsson@users.noreply.github.com>
2020-09-09 21:21:30 +0200

    Add files via upload

---
+++ Uppg6/Uppg6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Uppg6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double answer;
+            Console.WriteLine("Write N (störst)");
+            double n = double.Parse(Console.ReadLine());
+            Console.WriteLine("Write X (minst)");
+            double x = double.Parse(Console.ReadLine());
+
+            for (int i = 1; i < n; i++)
+            { 
+                n = ((n *= i) / Math.Pow(x, n));
+            }
+            answer = n + 1;
+            Console.WriteLine(answer);
+        }
+    }
+}
---
+++ Uppg7/Uppg7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Uppg7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            int fibb1 = 0;
+            int fibb2 = 1;
+            int fibb3;
+            int answer = 0;
+            Console.WriteLine("Write N");
+            n = int.Parse(Console.ReadLine());
+            Console.Write("0 1 ");
+            for (int i = 2; i < n; i++)
+            {
+                fibb3 = fibb1 + fibb2;
+                Console.Write($"{fibb3} ");
+                fibb1 = fibb2;
+                fibb2 = fibb3;
+                answer += fibb3;
+            }
+            Console.WriteLine();
+            Console.WriteLine($"The sum is : {answer + 1}");
+        }
+    }
+}
